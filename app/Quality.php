285ea79cd6dc80448598099e498fd9692fc8bf8e
Arno DUBOIS <arno.du@orange.fr>
2018-05-22 13:20:57 +0200

    Add fillables

--- app/Quality.php
+++ app/Quality.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Quality extends Model
 {
     protected $table = 'qualities';
+
+    protected $fillable = [
+        'quality'
+    ];
+
+    public $timestamps = false;
 }
--- app/Software.php
+++ app/Software.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Software extends Model
 {
     protected $table = 'softwares';
+
+    protected $fillable = [
+        'software'
+    ];
+
+    public $timestamps = false;
 }
