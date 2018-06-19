0fd423bb5c02438bdf7700b382cc6e991c8e8897
Arno DUBOIS <arno.du@orange.fr>
2018-06-19 11:16:04 +0200

    Integrating offers display

--- resources/views/offer/offers.blade.php
+++ resources/views/offer/offers.blade.php
@@ -2,39 +2,39 @@
 
 @section('content')
 
-<div class="container">
-    @if(Auth::user() && Auth::user()->enterprise !== "")
-    <div class="row">
-        <div class="row">
-            <a href="{{ route('offers.create') }}"><i class="material-icons icon-in-link">note_add</i> Créer une offre</a>
-        </div>
-    </div>
-    @endif
-    <div class="row">
-    @if($offers->isEmpty())
-        <p>Oups, il n'y aucune offre disponible... Revenez un peu plus tard !</p>
-    @else
-    @foreach($offers as $offer)
-        <a class="offer-link" href="{{ '/offers/'.$offer->id }}">
-        <div class="card card-horizontal-md-up card-offer">
-            <img class="card-img" src="{{ asset($offer->enterprise->logourl) }}" alt="{{ $offer->enterprise->name }}">
-            <div class="card-block">
-                <h5 class="card-title">{{$offer->name}}</h5>
-                <p class="card-text">{{ $offer->description }}</p>
-                <br>
-                {{ $offer->enterprise->name  }}
+    <div class="container">
+        @if(Auth::user() && Auth::user()->enterprise !== "")
+            <div class="row">
+                <div class="row">
+                    <a href="{{ route('offers.create') }}"><i class="material-icons icon-in-link">note_add</i> Créer une
+                        offre</a>
+                </div>
             </div>
+        @endif
+        <div class="row large">
+            @if($offers->isEmpty())
+                <p>Oups, il n'y aucune offre disponible... Revenez un peu plus tard !</p>
+            @else
+                @foreach($offers as $offer)
+                    <a class="offer-link" href="{{ '/offers/'.$offer->id }}">
+                        <div class="card card-horizontal card-offer">
+                            <img class="card-img" src="{{ asset($offer->enterprise->logourl) }}"
+                                 alt="{{ $offer->enterprise->name }}">
+                            <div class="card-text-offer">
+                                <h5 class="card-title">{{$offer->name}}</h5>
+                                <p class="card-description">{{ str_limit($offer->description) }}</p>
+                            </div>
+                            <h2 class="name-firm">{{ $offer->enterprise->name  }}</h2>
+                        </div>
+                    </a>
 
-        </div>
-        </a>
-
-    @endforeach
+                @endforeach
         </div>
 
         {{ $offers->links('offer.pagination') }}
-    @endif
+        @endif
 
-</div>
+    </div>
 
 
 @endsection
